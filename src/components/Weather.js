b2b45c60f30fa8ab640edd231234b40b74da9d46
adrian.hernandez1993@yahoo.com <adrianhernandez@Adrians-MBP-2.lan>
2022-12-08 18:56:17 -0800

    Lab 07 features

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,19 +1,22 @@
 import React from 'react';
-
+import Card from 'react-bootstrap/Card'
 
 class Weather extends React.Component {
     render() {
         console.log(this.props.data);
         return (
             // <div>{this.props.data}</div>
-            
+
             this.props.data.map((day, idx) => (
-                <div key={idx}>
-                <p>Date: {day.date}</p>
-                <p>Weather: {day.description}</p>
-                </div>
+                <div>
+                    <Card key={idx} style={{width: '18rem'}}>
+                        <Card.Title></Card.Title>
+                    <Card.Body>Date: {day.date}</Card.Body>
+                    <Card.Body>Weather: {day.description}</Card.Body>
+                    </Card>
+                </div >
             ))
-      )
+        )
 
     }
 }
